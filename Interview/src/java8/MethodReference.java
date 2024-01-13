263c8bd9f3825d2cb04d8f54c26c4e03120e920f
shubhamofficial2486 <45324404+shubhamofficial2486@users.noreply.github.com>
2024-01-13 08:10:46 +0530

    Added changes for method reference

---
+++ Interview/src/java8/MethodReference.java
@@ -0,0 +1,30 @@
+package java8;
+
+import java.util.Arrays;
+
+/**
+ * 4. **Method References:**
+   - Replace a lambda expression with a method reference for a specific scenario.
+   - Use a method reference to create a comparator for sorting a list.
+ */
+public class MethodReference {
+
+	public static void main(String[] args) {
+//		- Replace a lambda expression with a method reference for a specific scenario.
+		String[] array = {"Brahma", "Vishnu", "Shiva", "Ganesha", "Lakshmi", "Saraswati", "Krishna", "Rama", "Hanuman", "Durga","Maheshwara", "Neelakantha", "Rudra"};		
+		Arrays.sort(array ,(e1,e2) -> UtilityClass.compareByLength(e1,e2));		
+		Arrays.sort(array, UtilityClass::compareByLength);		
+		System.out.println("Sorted : "+Arrays.toString(array));
+		
+		
+
+	}
+
+}
+
+class UtilityClass {
+	
+	static int compareByLength(String s1, String s2) {
+		return Integer.compare(s1.length(), s2.length());
+	}
+}
